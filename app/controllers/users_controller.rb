ced780026ee68b21c02e2227030b9341495b7d69
q1umi-ooki <q1umi-ooki>
2019-04-20 14:15:22 +0900

    To make it fine viewing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
+  def index 
+    @group = Group.find(params[:group_id])
+    @members = @group.users
+    @searchedUsers = User.where('name LIKE(?)', "#{params[:name]}%") #.where.not(id: current_user.id)
+    @users = @searchedUsers - @members
+      respond_to do |format|
+          format.html 
+          format.json
+      end 
+  end
+
   def edit
   end
 
